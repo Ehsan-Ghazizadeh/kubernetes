286c8bc47eaefa11a4acf9be709672b9ec1b5ca9
Ehsan Ghazizadeh <ehsan@cafebazaar.cloud>
2023-09-06 00:58:41 +0330

    added script files.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # kubernetes
-Setup a self managed kubernetes cluster for training.
+
+Setup a self managed kubernetes cluster with containerd for training.
+
+To check cluster components:  
+`kubectl get --raw='/readyz?verbose'`
+
+After installation of kubernetes in order to be able to monitor resources of run this command:  
+`kubectl appy -f metrics.yaml`
+
+Use below commands to check pod and node resource usage:
+
+`kubectl top nodes`  
+`kubectl top nodes -n kube-system`  
+`kubectl top pods -n kube-system`
+
+To install latets helm chart run these commands as root:
+
+`curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3`  
+`chmod 700 get_helm.sh`  
+`./get_helm.sh`
--- master.sh
+++ master.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 
 # If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
-PUBLIC_IP_ACCESS="true"
+PUBLIC_IP_ACCESS="false"
 NODENAME=$(hostname -s)
 POD_CIDR="192.168.0.0/16"
 
@@ -18,13 +18,13 @@ sudo kubeadm config images pull
 
 if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
-    MASTER_PRIVATE_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
-    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    MASTER_PRIVATE_IP=$(ip addr show ens192 | awk '/inet / {print $2}' | cut -d/ -f1)
+    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" 
 
 elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
 
     MASTER_PUBLIC_IP=$(curl ipinfo.io/ip && echo "")
-    sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" 
 
 else
     echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
@@ -36,6 +36,9 @@ fi
 mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+
+# or run this instead:
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
 # Install Claico Network Plugin Network 
 
